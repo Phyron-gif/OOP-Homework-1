f0594a161c4b84b3804a4ef5b5b8a8daac68fedf
Tracy Phyron <tracy.jinor@ashesi.edu.gh>
2025-09-22 16:24:26 +0000

    checkpoint1

---
+++ FootballPlayer.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+public class FootballPlayer {
+
+	public static void main(String[] args){
+		/*String name;
+		int age;
+		double height;
+		double weight;*/
+		
+		Scanner player_details = new Scanner(System.in);
+		System.out.println("Enter your name: ");
+		String name = player_details.nextLine();
+		System.out.println("Enter your age: ");
+		int age = player_details.nextInt();
+		System.out.println("Enter your height in meters : ");
+		double height = player_details.nextDouble();
+		System.out.println("Enter your weight in pounds: ");
+		double weight = player_details.nextDouble();
+		System.out.println("Enter your jersey number: ");
+		int jersey_number = player_details.nextInt();
+		double pound =  0.45359237 ;
+		int meter = 100;
+		double weight_in_pound = weight/pound;
+		double height_in_centimeters = height*meter;
+		int final_weight = (int)weight_in_pound;
+		int final_height = (int)height_in_centimeters;
+		System.out.println("Player Name - " + name);
+		System.out.println("Age - " + age);
+		System.out.println("Height in centimeters - " + final_height + "cm");
+		System.out.println("Weight in kilograms - " + final_weight+"kg");
+		System.out.println("Jersey Number - " + jersey_number);
+		age++;
+		jersey_number--;
+		System.out.println("New Age - " + age );
+		System.out.println("Jersey_number - " + jersey_number);
+		if (age >= 18 && final_weight < 90) {
+			System.out.println("You are eligible to play");
+		}
+		else {
+			System.out.println("You are not eligible to play");
+			
+		}
+		if ((age < 18 ||weight < 90 )|| (age >= 18 || weight >= 90 )){
+           System.out.println("Player has a problem(either too young or too heavy)");
+				
+		}
+		if (!(age >= 18 && weight < 90)) {
+			System.out.println("Not Eligible");
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
